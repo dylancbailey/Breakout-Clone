443d85d27ac183abfd6f9c8c2f60d6b34100aa2a
dylancbailey <8r7.uTa!>
2016-05-24 16:24:02 -0400

    paddle input and collisions

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 var game = new Phaser.Game(480, 320, Phaser.AUTO, null,
     {preload: preload, create: create, update: update});
 var ball;
+var paddle;
 
 function preload() {
 
@@ -13,8 +14,9 @@ function preload() {
   // Add the background color
   game.stage.backgroundColor = "#eee";
 
-  // Load the ball
+  // Load the images
   game.load.image('ball', 'assets/ball.png');
+  game.load.image('paddle', 'assets/paddle.png');
 }
 
 function create() {
@@ -23,19 +25,35 @@ function create() {
   game.physics.startSystem(Phaser.Physics.ARCADE);
 
   // Create the ball
-  ball = game.add.sprite(50, 50, 'ball');
+  ball = game.add.sprite(game.world.width*0.5, game.world.height-25, 'ball');
+  // Set its anchor
+  ball.anchor.setTo(0.5);
   // Add physics to the ball
   game.physics.enable(ball, Phaser.Physics.ARCADE);
   // Allow the ball to collide with the edges of the screen
   ball.body.collideWorldBounds = true;
   // Enable bounce
   ball.body.bounce.set(1);
-  // Move the ball via velocity
-  ball.body.velocity.set(150, 150);
+  // Move the ball up via velocity
+  ball.body.velocity.set(150, -150);
+
+  // Create the paddle
+  paddle = game.add.sprite(game.world.width*0.5, game.world.height-5, 'paddle');
+  // Set the anchor to the middle
+  paddle.anchor.setTo(0.5, 1);
+  // Enable physics on the paddle
+  game.physics.enable(paddle, Phaser.Physics.ARCADE);
+  // Make paddle immovable
+  paddle.body.immovable = true;
 
 }
 
 function update() {
 
+  // Check for a collision between the ball and paddle
+  game.physics.arcade.collide(ball, paddle);
+
+  // User input for paddle and sets it to the middle on start
+  paddle.x = game.input.x || game.world.width*0.5;
 
 }
