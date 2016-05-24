d5dcec5bf8d974f6c9251f43220ccfdcd972e084
dylancbailey <8r7.uTa!>
2016-05-24 16:10:28 -0400

    Added ball and enabled physics

--- main.js
+++ main.js
@@ -1,2 +1,37 @@
+// Global variables
 var game = new Phaser.Game(480, 320, Phaser.AUTO, null,
-                          {preload: preload, create: create, update: update});
+    {preload: preload, create: create, update: update});
+var ball;
+
+function preload() {
+
+  // Scales the game and aligns it to the middle
+  game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+  game.scale.pageAlignHorizontally = true;
+  game.scale.pageAlignVertically = true;
+
+  // Add the background color
+  game.stage.backgroundColor = "#eee";
+
+  // Load the ball
+  game.load.image('ball', 'assets/ball.png');
+}
+
+function create() {
+
+  // Create the physics
+  game.physics.startSystem(Phaser.Physics.ARCADE);
+
+  // Create the ball
+  ball = game.add.sprite(50, 50, 'ball');
+  // Add physics to the ball
+  game.physics.enable(ball, Phaser.Physics.ARCADE);
+  // Move the ball via velocity
+  ball.body.velocity.set(150, 150);
+
+}
+
+function update() {
+
+
+}
